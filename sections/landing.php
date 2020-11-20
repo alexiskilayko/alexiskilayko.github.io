907a4f98696c2295745487c1d2bff24f06a29bc7
Alexis Kilayko <alexis.kilayko@gmail.com>
2020-11-20 14:15:40 -0500

    Remove sectionphp files

--- sections/landing.php
+++
@@ -1,8 +0,0 @@
-<!-- LANDING -->	
-<div id="landing" class="container-fluid animate1" style="background-image: url('imgs/me.JPG'); background-repeat: no-repeat; background-position: center; background-size: cover;">
-	<div class="row align-items-center h-100">
-		<div class="col-12">
-			<h1 class="animate2">A developer centered on compassion.</h1>
-		</div>
-	</div>
-</div>
